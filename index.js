d3487f45f6fcc428b6ab6cbec602297f92f096f5
fraten <francescopolidoro10@gmail.com>
2025-01-02 16:24:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["about to CRASHOUT!!1", "CRASHING OUT on these YNs"];
+const statusMessages = ["crashing out", "#freekingvon"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
